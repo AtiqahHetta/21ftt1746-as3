79761cab353d0cf992d21701d348358e6f53d74c
AtiqahHetta <21ftt1746@student.pb.edu.bn>
2023-04-11 10:55:48 +0800

    change the color and text

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="max-w-md mx-auto bg-sky-100 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
   <div class="md:flex">
     <div class="md:shrink-0">
-      <img class="h-48 w-full object-cover md:h-full md:w-48" src="atiqahrasyimah.JPEG" alt="Modern building architecture">
+      <img class="h-50 w-100 object-cover md:h-full md:w-60" src="atiqahrasyimah.JPEG" alt="Modern building architecture">
     </div>
     <div class="p-8">
       <br>
       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"> Atiqah Rasyimah </div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-      <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"> 21ftt1746@student.pb.edu.bn </a>
+      <br>
+      <p class="mt-2 text-slate-500"> I'm a student from Politeknik Brunei, taking Level 5 in Web Development.  </p>
     </div>
   </div>
 </div>
