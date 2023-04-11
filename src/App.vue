5dbdbbb19ca812d0629ce994be39c7ced6f9814c
AtiqahHetta <21ftt1746@student.pb.edu.bn>
2023-04-11 12:04:22 +0800

    change title

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@ import HelloWorld from './components/HelloWorld.vue'
   <div class="max-w-md mx-auto bg-sky-100 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
   <div class="md:flex">
     <div class="md:shrink-0">
-      <img class="h-50 w-100 object-cover md:h-full md:w-60" src="atiqahrasyimah.JPEG" alt="Modern building architecture">
+      <img class="h-40 w-50 object-cover md:h-full md:w-60 mx-auto" src="atiqahrasyimah.JPEG" alt="">
     </div>
     <div class="p-8">
       <br>
       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"> Atiqah Rasyimah </div>
       <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"> 21ftt1746@student.pb.edu.bn </a>
       <br>
-      <p class="mt-2 text-slate-500"> I'm a student from Politeknik Brunei, taking Level 5 in Web Development.  </p>
+      <p class="mt-2 text-black"> I'm a student from Politeknik Brunei, taking Level 5 Diploma in Web Development.  </p>
     </div>
   </div>
 </div>
