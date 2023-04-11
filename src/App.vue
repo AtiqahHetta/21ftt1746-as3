853658083c9150270aeb0997488f7c1497fb88a8
AtiqahHetta <21ftt1746@student.pb.edu.bn>
2023-04-11 10:54:39 +0800

    delete br

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img class="h-48 w-full object-cover md:h-full md:w-48" src="atiqahrasyimah.JPEG" alt="Modern building architecture">
     </div>
     <div class="p-8">
+      <br>
       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold"> Atiqah Rasyimah </div>
       <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
       <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
